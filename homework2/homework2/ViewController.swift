d51d5d9733fe109e5ab60c89ea2f3eec789217c2
Jeffery Ho <chiachun2491@gmail.com>
2020-03-27 16:17:13 +0800

    [Update] homework2

---
+++ homework2/homework2/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  homework2
+//
+//  Created by Jeffery Ho on 2020/3/26.
+//  Copyright © 2020 Jeffery Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var textLabel: UILabel!
+    
+    var imageName:String?
+    var itemName:String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        segueDataProcess(imageName: imageName, itemName: itemName)
+    }
+    
+    func segueDataProcess(imageName:String?, itemName:String?) {
+        textLabel.text = "飲料名稱：\(itemName!)"
+        image.image = UIImage(named: imageName!)
+    }
+
+
+}
+
